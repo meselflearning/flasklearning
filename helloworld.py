8617eba28a7c02976468233bdf9f65f57e8b03a4
meselflearning <meselflearning@gmail.com>
2017-10-16 10:18:35 +0000

    added html tag

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ def index():
     return '<h1>Hello world</h1>'
 @app.route('/<name>')
 def user(name):
-    return '<h1>Hello {}</h1>'.format(name)
+    return '<h1>Hello {} welcome to the flask world</h1>'.format(name)
 if __name__ == '__main__':
     app.run(debug=True)
 
